d3a98cdb5e361e62777921f0d275859e44d15aa0
Chengxiu Luo <cxluo@thoughtworks.com>
2018-04-11 16:11:25 +0800

    add file loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Picture from './picture.jpg';
 
 function component() {
   var element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。
+  var myPicture = new Image();
+  myPicture.src = Picture;
+
+  element.appendChild(myPicture);
 
   return element;
 }
